583b52ba747d89da3af61b51e04f488624db549a
Maksym Husar <maksym.husar@brander.ua>
2018-05-21 10:20:02 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '10.0'
+
+target 'SunriseSunsetApp' do
+  use_frameworks!
+
+  # Pods for SunriseSunsetApp
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  
+end
---
+++ SunriseSunsetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunriseSunsetApp
+//
+//  Created by Maksym Husar on 5/21/18.
+//  Copyright © 2018 Maksym Husar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
